6b9eda78348145d2aecac588844aab0adc99515f
Matheus Alano Joenck <matheusalanojoenck@gmail.com>
2019-05-28 13:04:00 -0300

    Update BFS.java
    comentários explicando BFS

--- Grafos/src/grafos/BFS.java
+++ Grafos/src/grafos/BFS.java
@@ -11,9 +11,9 @@ import java.util.LinkedList;
 public class BFS {
     
     private ArrayList<ArrayList<Integer>> listaAdj;
-    private final int V;
-    private int[][] arvoreGeradora;
-    private int tempo = 0;
+    private final int V; //Quantidade de vertices
+    private int[][] arvoreGeradora; //Vetor para guardar os vertices em ordem de visitação, [Origem, Destino]
+    private int tempo = 0; //Quanto um vertice entrou na fila
     
     public BFS(ArrayList<ArrayList<Integer>> listaAdj){
         this.listaAdj = listaAdj;
@@ -22,27 +22,47 @@ public class BFS {
     }
     
     public void bfs(int vertice){
+        //Todos os vertices são marcados como não visitados
         boolean[] visitado = new boolean[V];
+
+        //Vetor que guarda o tempo de entrada de cada vértice;
         int[] tempoEntrada = new int [V];
 
+        //Uma fila para o BFS
         LinkedList<Integer> fila = new LinkedList<>();
 
+        //Marca o vertice atual como visitado
         visitado[vertice]=true;
+        //Adiciona o vetice na fila
         fila.add(vertice);
-        tempoEntrada[vertice] = tempo + 1;
-  
+        //Marca o tempo de entrada
+        tempoEntrada[vertice] = tempo + 1; //O tempo inicia em zero, para questão de visialização é adicionado + 1 ao tempo para melhor entendimento
+
+        //Repetir enquanto a fila não estiver vazia
         while (!fila.isEmpty()) {
 
+            //Tira o primeiro vertice da fila
             vertice = fila.poll();
 
+            //Para todos os vertice adjacentes ao vertice removido da fila:
+            //Marcar como visitado os vertices não visitados e os adicionar na fila
             for (int n : listaAdj.get(vertice)) {
-
                 if (!visitado[n]) {
+                    //Adicionando o vertice ao vetor, a variavel tempo atual como index para o vetor
+                    //vertice é a origem da aresta
                     arvoreGeradora[tempo][0] = vertice;
+
+                    //Marcando o vertice adjacente como visitado
                     visitado[n] = true;
+                    //Adicionando o vertice na fila
                     fila.add(n);
+
+                    //O vertice adjacente n, é o destino da aresta
                     arvoreGeradora[tempo][1] = n;
+
+                    //Aumenta o tempo das entradas
                     tempo++;
+                    //Adiciona ao vertice o seu tempo de entrada na fila
                     tempoEntrada[n] = tempo + 1;
                 }
             } 
